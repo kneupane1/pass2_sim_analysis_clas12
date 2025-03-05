47cd33dca1035189b2929e831b79f28757a73965
Krishna Neupane <35571116+kneupane1@users.noreply.github.com>
2025-03-05 06:27:42 -0500

    checking for background in excl topo

--- exe/clas12_analysis.hpp
+++ exe/clas12_analysis.hpp
@@ -719,7 +719,7 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram> &_hi
 
                                                                                         if ((event->MM2_exclusive() < -0.004 || event->MM2_exclusive() > 0.002) &&
                                                                                             (event->MM2_mpip() < -0.024 || event->MM2_mpip() > 0.079) &&
-                                                                                            (event->MM2_mprot() < -0.79 || event->MM2_mprot() > 0.1025))
+                                                                                            (event->MM2_mprot() < 0.79 || event->MM2_mprot() > 0.1025))
 
                                                                                         // if (_hists->MM_cut(event->W(), event->Q2(), event->MM2_mPim()))
                                                                                         {
